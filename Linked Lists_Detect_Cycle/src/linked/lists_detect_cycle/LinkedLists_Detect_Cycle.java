aa69507ae77ffe34ba664d4ff540978224aba3c7
osamamohsen <osamamohsen1994@gmail.com>
2017-03-20 00:26:03 +0200

    Add Detect Cycle

--- Linked Lists_Detect_Cycle/src/linked/lists_detect_cycle/LinkedLists_Detect_Cycle.java
+++ Linked Lists_Detect_Cycle/src/linked/lists_detect_cycle/LinkedLists_Detect_Cycle.java
@@ -9,26 +9,58 @@ package linked.lists_detect_cycle;
  *
  * @author osama
  */
-public class LinkedLists_Detect_Cycle {
+import java.io.*;
+import java.util.*;
+class Node {
+    int data;
+    Node next;
 
-    class Node {
-        int data;
-        Node next;
-    }
+    Node(int d){
+        data = d;
+        next = null;
+    } 
+}
+
+public class LinkedLists_Detect_Cycle {
     /**
      * @param args the command line arguments
      */
-    boolean hasCycle(Node head) {
-        if(head == null || head.next == null || head.data==0) {return false;}
-        else if(head.data != 0){
-            head.data = 0;
-            head = head.next;
-            return true;
+    static boolean hasCycle(Node head) {
+        if(head == null) {return false;}
+//        boolean [] visited = new boolean[101];   
+        Node fast = head.next,slow = head;
+        while(fast != null && fast.next !=null && slow != null){
+            if(fast == slow) return true;
+            fast = fast.next.next;
+            slow = slow.next;
         }
         return false;
     }
     
+    public static Node insert(Node head,int data){
+        if(head==null){
+            head = new Node(data);
+        }
+        else{
+            Node n = head;
+            while(n.next != null){
+                n = n.next;
+            }
+            n.next =  new Node(data);
+        }
+        return head;
+    }
+    
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        Node head = null;
+        while(x-- > 0){
+            int w = s.nextInt();
+            head = insert(head, w);
+        }
+        System.out.println(hasCycle(head));
+        
     }
     
 }
